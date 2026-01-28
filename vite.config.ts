26d1831dcb11b3ba9f8ae8d20dff1e59bf888273
uktamov99 <ayozo9900@gmail.com>
2026-01-28 18:28:23 +0500

    Initial commit: Presto Pizza Frontend

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // SHU QATORNI QO'SHING:
+      base: '/pitsa_sayt/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
